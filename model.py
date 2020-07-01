ddbdc0612d9f83b99fb044dcc2398fa22cce3571
issacto <54349664+issacto@users.noreply.github.com>
2020-07-01 23:41:20 +0800

    sidan

--- model.py
+++ model.py
@@ -3,23 +3,50 @@
 # Deadline: 5 July 2020
 # Energy Demand Prediction
 
-# pytorch mlp for binary classification
-from csvDataset import CSVDataset
+# pytorch mlp for regression
 from numpy import vstack
-from sklearn.metrics import accuracy_score
+from numpy import sqrt
+from pandas import read_csv
+from sklearn.metrics import mean_squared_error
+from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
-
+from torch.utils.data import random_split
 from torch import Tensor
 from torch.nn import Linear
-from torch.nn import ReLU
 from torch.nn import Sigmoid
 from torch.nn import Module
 from torch.optim import SGD
-from torch.nn import BCELoss
-from torch.nn.init import kaiming_uniform_
+from torch.nn import MSELoss
 from torch.nn.init import xavier_uniform_
-
-
+ 
+# dataset definition
+class CSVDataset(Dataset):
+    # load the dataset
+    def __init__(self, path):
+        # load the csv file as a dataframe
+        df = read_csv(path, header=None)
+        # store the inputs and outputs
+        self.X = df.values[:, :-1].astype('float32')
+        self.y = df.values[:, -1].astype('float32')
+        # ensure target has the right shape
+        self.y = self.y.reshape((len(self.y), 1))
+ 
+    # number of rows in the dataset
+    def __len__(self):
+        return len(self.X)
+ 
+    # get a row at an index
+    def __getitem__(self, idx):
+        return [self.X[idx], self.y[idx]]
+ 
+    # get indexes for train and test rows
+    def get_splits(self, n_test=0.33):
+        # determine sizes
+        test_size = round(n_test * len(self.X))
+        train_size = len(self.X) - test_size
+        # calculate the split
+        return random_split(self, [train_size, test_size])
+ 
 # model definition
 class MLP(Module):
     # define model elements
@@ -27,33 +54,29 @@ class MLP(Module):
         super(MLP, self).__init__()
         # input to first hidden layer
         self.hidden1 = Linear(n_inputs, 10)
-        kaiming_uniform_(self.hidden1.weight, nonlinearity='relu')
-        self.act1 = ReLU()
+        xavier_uniform_(self.hidden1.weight)
+        self.act1 = Sigmoid()
         # second hidden layer
         self.hidden2 = Linear(10, 8)
-        kaiming_uniform_(self.hidden2.weight, nonlinearity='relu')
-        self.act2 = ReLU()
+        xavier_uniform_(self.hidden2.weight)
+        self.act2 = Sigmoid()
         # third hidden layer and output
         self.hidden3 = Linear(8, 1)
         xavier_uniform_(self.hidden3.weight)
-        self.act3 = Sigmoid()
-
+ 
     # forward propagate input
     def forward(self, X):
         # input to first hidden layer
         X = self.hidden1(X)
         X = self.act1(X)
-        # second hidden layer
+         # second hidden layer
         X = self.hidden2(X)
         X = self.act2(X)
         # third hidden layer and output
         X = self.hidden3(X)
-        X = self.act3(X)
         return X
-
+ 
 # prepare the dataset
-
-
 def prepare_data(path):
     # load the dataset
     dataset = CSVDataset(path)
@@ -63,13 +86,11 @@ def prepare_data(path):
     train_dl = DataLoader(train, batch_size=32, shuffle=True)
     test_dl = DataLoader(test, batch_size=1024, shuffle=False)
     return train_dl, test_dl
-
+ 
 # train the model
-
-
 def train_model(train_dl, model):
     # define the optimization
-    criterion = BCELoss()
+    criterion = MSELoss()
     optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9)
     # enumerate epochs
     for epoch in range(100):
@@ -85,10 +106,8 @@ def train_model(train_dl, model):
             loss.backward()
             # update model weights
             optimizer.step()
-
+ 
 # evaluate the model
-
-
 def evaluate_model(test_dl, model):
     predictions, actuals = list(), list()
     for i, (inputs, targets) in enumerate(test_dl):
@@ -98,19 +117,17 @@ def evaluate_model(test_dl, model):
         yhat = yhat.detach().numpy()
         actual = targets.numpy()
         actual = actual.reshape((len(actual), 1))
-        # round to class values
-        yhat = yhat.round()
         # store
         predictions.append(yhat)
         actuals.append(actual)
     predictions, actuals = vstack(predictions), vstack(actuals)
-    # calculate accuracy
-    acc = accuracy_score(actuals, predictions)
-    return acc
-
+    # calculate msep
+    for i in range(len(predictions)):
+            print(predictions[i] + " VS actual -"+ actuals[i])
+    mse = mean_squared_error(actuals, predictions)
+    return mse
+ 
 # make a class prediction for one row of data
-
-
 def predict(row, model):
     # convert row to data
     row = Tensor([row])
@@ -119,10 +136,9 @@ def predict(row, model):
     # retrieve numpy array
     yhat = yhat.detach().numpy()
     return yhat
-
-
+ 
 # prepare the data
-path = "datasets-48149-87794-PJM_Load_hourly.csv"
+path = 'window.csv'
 train_dl, test_dl = prepare_data(path)
 print(len(train_dl.dataset), len(test_dl.dataset))
 # define the network
@@ -130,12 +146,5 @@ model = MLP(3)
 # train the model
 train_model(train_dl, model)
 # evaluate the model
-acc = evaluate_model(test_dl, model)
-print('Accuracy: %.3f' % acc)
-# make a single prediction (expect class=1)
-
-##################
-
-row = []
-yhat = predict(row, model)
-print('Predicted: %.3f (class=%d)' % (yhat, yhat.round()))
+mse = evaluate_model(test_dl, model)
+print('MSE: %.3f, RMSE: %.3f' % (mse, sqrt(mse)))

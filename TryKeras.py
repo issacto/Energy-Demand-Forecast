2ee7c992d86fdd148386bf4ab6375e3034f79ba0
issacto <54349664+issacto@users.noreply.github.com>
2020-07-03 00:55:32 +0800

    update

--- TryKeras.py
+++ TryKeras.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import numpy
 import matplotlib.pyplot as plt
 from pandas import read_csv
@@ -54,3 +55,66 @@ plt.plot(dataset)
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
 plt.show()
+=======
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from keras.layers import Dropout
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+
+
+#Variables
+dataset=np.loadtxt("/Users/issac/Documents/GitHub/Energy-Demand-Forecast/window.csv", delimiter=",")
+x=dataset[:,0:3]
+y=dataset[:,3]
+y=np.reshape(y, (-1,1))
+scaler_x = MinMaxScaler()
+scaler_y = MinMaxScaler()
+print(scaler_x.fit(x))
+xscale=scaler_x.transform(x)
+print(scaler_y.fit(y))
+yscale=scaler_y.transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(xscale, yscale, test_size = 0.3, random_state = 0)
+
+model = Sequential()
+model.add(Dense(12, input_dim=3, kernel_initializer='normal', activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(8, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1, kernel_initializer='normal'))
+model.summary()
+model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
+history = model.fit(X_train, y_train, epochs=120, batch_size=32,  verbose=1, validation_split=0.2)
+test_prediction_array =[]
+for x in range(len(X_test)):
+    Xnew = np.array([X_test[x]])
+    ynew= model.predict(Xnew)
+    #invert normalize
+    ynew = scaler_y.inverse_transform(ynew) 
+    Xnew = scaler_x.inverse_transform(Xnew)
+    test_prediction_array.append(ynew[0])
+    print("X=%s, Predicted=%s" % (Xnew[0], ynew[0]))
+
+test_actual_data = scaler_y.inverse_transform(y_test) 
+plt.plot(test_prediction_array[0:100])
+plt.plot(test_actual_data[0:100])
+plt.title('prediction vs actual')
+plt.ylabel('value')
+plt.xlabel('epoch')
+plt.legend(['prediction', 'actual'], loc='upper left')
+plt.show()
+
+#z =[]
+#for i in range(len(test_prediction_array)):
+    #z.append(abs(test_prediction_array[i]-test_actual_data[i])/test_actual_data[i])
+#plt.plot(z)
+#plt.show()
+>>>>>>> Stashed changes
